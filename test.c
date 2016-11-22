6469c12a652922360c02e957f72dbbc67c60fa74
Andy Grover <agrover@redhat.com>
2016-11-21 16:28:06 -0800

    add polling and status printing

--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <poll.h>
 
 #include <libdevmapper.h>
 
@@ -73,16 +74,52 @@ int set_dev_event(struct dm_dev *dev)
 	snprintf(ioc.name, sizeof(ioc.name), "%s", dev->dm_name);
 
 	if (ioctl(dev->fd, DM_DEV_EVENT_SET, &ioc) < 0) {
-		printf("error: %m");
+		printf("error: %m\n");
 		return -1;
 	}
 
 	return 0;
 }
 
+int print_status(char* dm_name)
+{
+	struct dm_task *task;
+	void *next = NULL;
+	int r = 1;
+	uint64_t start, length;
+	char *type = NULL;
+	char *params = NULL;
+
+
+	if (!(task = dm_task_create(DM_DEVICE_STATUS)))
+		return 0;
+
+	if (!dm_task_set_name(task, dm_name))
+		return 0;
+
+	if (!dm_task_run(task)) {
+		r = 0;
+		goto out;
+	}
+
+	/* Fetch targets and print 'em */
+	do {
+		next = dm_get_next_target(task, next, &start, &length,
+					  &type, &params);
+		printf("status: %" PRIu64 " %" PRIu64 " %s %s\n",
+		       start, length, type, params);
+
+	} while (next);
+
+out:
+	dm_task_destroy(task);
+	return r;
+}
+
 int main()
 {
 	struct dm_dev *dev;
+	struct pollfd *pollfds;
 	int fd;
 
 	get_names();
@@ -97,6 +134,7 @@ int main()
 		dev->fd = fd;
 
 		printf("dm name = %s fd = %d\n", dev->dm_name, dev->fd);
+		print_status(dev->dm_name);
 
 		if (set_dev_event(dev) < 0) {
 			printf("set_dev_event failed\n");
@@ -104,6 +142,35 @@ int main()
 		}
 
 	}
+
+	pollfds = calloc(sizeof(*pollfds), darray_size(dm_devs));
+	if (!pollfds) {
+		printf("could not alloc pollfds[], running this on a wristwatch???\n");
+		return 1;
+	}
+
+	do {
+		int i = 0;
+		darray_foreach(dev, dm_devs) {
+			pollfds[i].fd = dev->fd;
+			pollfds[i].events = POLLIN;
+			pollfds[i].revents = 0;
+			i++;
+		}
+
+		printf("entering poll()\n");
+		poll(pollfds, darray_size(dm_devs), -1);
+		printf("returned from poll()\n");
+
+		i = 0;
+		darray_foreach(dev, dm_devs) {
+			if (pollfds[i].revents & POLLIN) {
+				printf("got POLLIN\n");
+				print_status(dev->dm_name);
+			}
+			i++;
+		}
+	} while (1);
 
 	return 0;
 }
